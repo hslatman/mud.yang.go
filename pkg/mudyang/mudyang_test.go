b876fc7b608864744a4eaa9af8b83fc5196a846c
Herman Slatman <hermanslatman@hotmail.com>
2023-10-16 13:22:48 +0200

    Add more test cases

--- pkg/mudyang/mudyang_test.go
+++ pkg/mudyang/mudyang_test.go
@@ -1,28 +1,111 @@
 package mudyang_test
 
 import (
+	"encoding/json"
 	"os"
 	"testing"
 
-	"github.com/hslatman/mud.yang.go/pkg/mudyang"
+	"github.com/openconfig/ygot/ygot"
 	"github.com/openconfig/ygot/ytypes"
-
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/hslatman/mud.yang.go/pkg/mudyang"
 )
 
-func TestValidate(t *testing.T) {
-	json, err := os.ReadFile("./../../examples/amazonEchoMud.json")
-	require.NoError(t, err)
-
-	mud := &mudyang.Mudfile{}
-	err = mudyang.Unmarshal([]byte(json), mud)
-	require.NoError(t, err)
-
-	options := &ytypes.LeafrefOptions{
+func TestMudfile_ΛValidate(t *testing.T) {
+	t.Parallel()
+	leafRefOptions := &ytypes.LeafrefOptions{
 		IgnoreMissingData: false,
 		Log:               true,
 	}
-	err = mud.ΛValidate(options)
-	assert.NoError(t, err)
+	type args struct {
+		opts []ygot.ValidationOption
+	}
+	tests := []struct {
+		name              string
+		filepath          string
+		args              args
+		wantUnmarshalErr  bool
+		wantValidationErr bool
+	}{
+		{
+			name:     "ok/amazon-echo",
+			filepath: "./../../examples/amazonEchoMud.json",
+			args: args{
+				opts: []ygot.ValidationOption{leafRefOptions},
+			},
+		},
+		{
+			name:     "ok/lightbulb-2000",
+			filepath: "./../../examples/lightbulb2000.json",
+			args: args{
+				opts: []ygot.ValidationOption{leafRefOptions},
+			},
+		},
+		{
+			name:     "ok/wemo-switch",
+			filepath: "./../../examples/wemoswitchMud.json",
+			args: args{
+				opts: []ygot.ValidationOption{leafRefOptions},
+			},
+		},
+		{
+			name:     "ok/sbom/mixed",
+			filepath: "./../../examples/sbom/mixedExample.json",
+			args: args{
+				opts: []ygot.ValidationOption{leafRefOptions},
+			},
+		},
+		{
+			name:     "ok/sbom/complete",
+			filepath: "./../../examples/sbom/completeExample.json",
+			args: args{
+				opts: []ygot.ValidationOption{leafRefOptions},
+			},
+		},
+		{
+			name:     "ok/tls/example",
+			filepath: "./../../examples/tls/example.json",
+			args: args{
+				opts: []ygot.ValidationOption{leafRefOptions},
+			},
+		},
+		{
+			name:     "fail/amazon-echo",
+			filepath: "./../../examples/invalidAmazonEchoMud.json",
+			args: args{
+				opts: []ygot.ValidationOption{leafRefOptions},
+			},
+			wantUnmarshalErr: true,
+		},
+	}
+	for _, tt := range tests {
+		tc := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			data, err := os.ReadFile(tc.filepath)
+			require.NoError(t, err)
+
+			var v map[string]interface{}
+			err = json.Unmarshal(data, &v)
+			require.NoError(t, err)
+
+			mud := &mudyang.Mudfile{}
+			err = mudyang.Unmarshal(data, mud)
+			if tc.wantUnmarshalErr {
+				assert.Error(t, err)
+				return
+			}
+
+			err = mud.ΛValidate(tc.args.opts...)
+			if tc.wantValidationErr {
+				assert.Error(t, err)
+				return
+			}
+
+			assert.NoError(t, err)
+		})
+	}
 }
